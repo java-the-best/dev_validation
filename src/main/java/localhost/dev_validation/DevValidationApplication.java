f3817f97f140435bc299a7a48e1b0e998dba5f13
viktorov_maxim <victorov_maxim@mail.ru>
2022-11-17 19:33:01 +0300

    springBoot

---
+++ src/main/java/localhost/dev_validation/DevValidationApplication.java
@@ -0,0 +1,13 @@
+package localhost.dev_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/localhost/dev_validation/DevValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package localhost.dev_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
